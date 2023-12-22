82fc01911ea7648d811f9cea6be82928ac1333d9
Abrar Alam <abraralam246@gmail.com>
2023-12-22 16:47:40 -0500

    add prototype of the mouse over effect

---
+++ app.js
@@ -0,0 +1,44 @@
+const mem_sect = document.querySelector("section.memes-section");
+
+
+console.log(mem_sect);
+mem_sect.addEventListener("mouseover", function(e){
+
+    // if (e.target.)
+    
+    // if (((e.target.tagName === "DIV") && (e.target.getAttribute("class") === "image-container") )
+    //    ||  (e.target.tagName === "IMG") 
+    //    || ((e.target.tagName === "DIV") && (e.target.getAttribute("class") === ""))){
+    //     console.log("Hello");
+    // }
+    console.dir(e.target);
+
+    let idx = null;
+    let element = null;
+    if ((e.target.tagName === "IMG")){
+        e.target.className = "mouseover";
+        // id = e.target.parentElement.id;
+        // console.log("id: ", id);
+        // element = document.querySelector(`#${id}`);
+        // console.dir(element);
+        // element.querySelector("div.text-overlay-top").classList.add = "mouseover";
+        // element.querySelector("div.text-overlay-bottom").classList.add = "mouseover";
+        idx = Array.prototype.indexOf.call(e.target.parentElement.parentElement.children, e.target.parentElement);
+        console.dir(idx);
+        element = document.querySelectorAll("section.memes-section")[idx];
+        element.querySelector("div.text-overlay-top").classList.add("mouseover");
+        element.querySelector("div.text-overlay-bottom").classList.add("mouseover");
+        
+
+    }
+
+
+});
+
+mem_sect.addEventListener("mouseout", function(e){
+    console.dir(e.target);
+    if (e.target.tagName === "IMG"){
+        e.target.parentElement.className = "image-container";
+    }
+
+});
