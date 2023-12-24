e1b3446a0ad8f50b2cbaeea3218bb2d7575cdced
Abrar Alam <abraralam246@gmail.com>
2023-12-24 15:00:12 -0500

    add the draft buggy version to improve on

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const mem_sect = document.querySelector("section.memes-section");
-
+let selected_item_idx = null;
 
 console.log(mem_sect);
 mem_sect.addEventListener("mouseover", function(e){
@@ -11,6 +11,8 @@ mem_sect.addEventListener("mouseover", function(e){
     //    || ((e.target.tagName === "DIV") && (e.target.getAttribute("class") === ""))){
     //     console.log("Hello");
     // }
+
+
     console.dir(e.target);
 
     let idx = null;
@@ -25,6 +27,7 @@ mem_sect.addEventListener("mouseover", function(e){
         // element.querySelector("div.text-overlay-bottom").classList.add = "mouseover";
         idx = Array.prototype.indexOf.call(e.target.parentElement.parentElement.children, e.target.parentElement);
         console.dir(idx);
+        selected_item_idx = idx;
         element = document.querySelectorAll("section.memes-section")[idx];
         element.querySelector("div.text-overlay-top").classList.add("mouseover");
         element.querySelector("div.text-overlay-bottom").classList.add("mouseover");
@@ -38,8 +41,25 @@ mem_sect.addEventListener("mouseover", function(e){
 
 mem_sect.addEventListener("mouseout", function(e){
     console.dir(e.target);
+    let idx = null;
+    let element = null;
     if (e.target.tagName === "IMG"){
-        e.target.parentElement.className = "image-container";
+        // e.target.parentElement.className = "image-container";
+        e.target.className = "";
+
+        idx = Array.prototype.indexOf.call(e.target.parentElement.parentElement.children, e.target.parentElement);
+        console.dir(idx);
+
+        if ((idx !== selected_item_idx)){
+            element = document.querySelectorAll("section.memes-section")[idx];
+            element.querySelector("div.text-overlay-top").classList.remove("mouseover");
+            element.querySelector("div.text-overlay-bottom").classList.remove("mouseover");
+            element.querySelector("div.button-overlay").style.display = "none";
+        }
+            
+        
+        
+
     }
 
 });
